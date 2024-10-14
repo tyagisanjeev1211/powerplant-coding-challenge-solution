502888937f3aa88f154b86e22e93b30d69c687d8
Sanjeev Tyagi <110079993+tyagisanjeev1211@users.noreply.github.com>
2024-10-14 18:49:32 +0200

    Updated logic for residual electric load adjustment

--- app/src/production_plan.py
+++ app/src/production_plan.py
@@ -16,21 +16,34 @@ class Production_plan:
             d = {}
             d['name'] = plant['name']
 
-            if (plant["type"] == "windturbine" and self.load >= plant['pmax']):
-                ## wind-turbine with % wind 
-                d['p']    = plant['pmax'] * plant['rate'] / 100 if (plant['rate'] > 0) else plant['pmax']
-            
-            elif(self.load > 0 and self.load > plant['pmin']):
-                ## other fuel options 
-                d['p']    = min(plant['pmax'], self.load)
+            if (plant["type"] == "windturbine" and plant['rate'] > 0): 
+                plant['pmax'] = round(plant['pmax'] * plant['rate'] / 100,1)
+ 
+            if(self.load > 0 and self.load >= plant['pmin']):
+                d['p']    = round( min(plant['pmax'], self.load), 1)
             else:
-                ## If production target is completed, then other powerplants will have no target to generate power 
                 d['p'] = 0.0
 
             ## Subtract assigned load from targeted load 
-            self.load -=  d['p']
-
+            print("PLANT: ", d['name'], "LOAD: ", self.load, ", LESS: ",  d['p'])
+            self.load -= d['p']
 
             production_chart.append(d)
 
+        if( self.load > 0 ):
+            plant = self._residual_load_adjustment(production_chart)
+        
+            production_chart.append({plant['name'] : round(self.load,1) })
+
+        
         return production_chart
+
+    def _residual_load_adjustment(self, production_chart):
+
+        list_plants = None
+        for p in production_chart:
+            if (float(p['p']) == 0):
+                list_plants = [ d for d in self.powerplants if (d['name'] == p['name'] ) ]
+
+               # return a plant which has minimum pmin value 
+        return sorted(list_plants, key=lambda p: p['pmin'])[0]
